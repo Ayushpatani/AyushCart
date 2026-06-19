dabdc4273781b679f28988a3bc452869b9654085
Ayushpatani <jpatni099@gmail.com>
2026-06-19 23:06:19 +0530

    Added spring boot dependencies | Added first spring boot app

--- ayushcart-app/src/main/java/com/ayushcart/App.java
+++ ayushcart-app/src/main/java/com/ayushcart/App.java
@@ -1,13 +1,19 @@
 package com.ayushcart;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Writing my first spring boot program!!");
+        SpringApplication.run(App.class, args);
+        System.out.println("Spring boot application started!!");
     }
 }
